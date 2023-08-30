0c6720a927ac8593dcc0414f91aef6a8ef4e2bc4
Henrique Goxtoso <henrique.marqueti@estudante.ifms.edu.br>
2023-08-30 07:39:49 -0400

    commit atv tere

--- src/componentes/filme/Main.tsx
+++ src/componentes/filme/Main.tsx
@@ -3,7 +3,8 @@ function Body(){
     return(
         <div className="text">
             <p className="texto">Sou Henrique Gavazzoni Marqueti, o mais goxtoso do Brasil</p>
-            <img src="https://cdn-icons-png.flaticon.com/512/30/30237.png" alt="" />
+            
+            <img className="img-main" src="https://cdn-icons-png.flaticon.com/512/30/30237.png" alt="" />
         </div>
     )
 }
--- src/componentes/footer/Footer.tsx
+++ src/componentes/footer/Footer.tsx
@@ -4,7 +4,10 @@ function Footer(){
     return(
         <footer>
             <section>
-                <img id="logo" src={logo} alt="" />
+            <img id="logo" src={logo} alt="" />
+            </section>
+            <section>
+                
 
 <img src="https://static.vecteezy.com/system/resources/previews/016/716/469/original/instagram-icon-free-png.png" alt="" />
 <img src="https://cdn.icon-icons.com/icons2/1476/PNG/512/twitter_101809.png" alt="" />
@@ -14,8 +17,14 @@ function Footer(){
 
             <section>
             <a href="#">Termos de Serviço</a>
+            
             </section>
-           
+           <section>
+           <a href="#">Política de Privacidade</a>
+           </section>
+           <section>
+            <p className="texto-footer">@GoxtosoCompany</p>
+           </section>
         </footer>
     )
 }
