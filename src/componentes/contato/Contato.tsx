2ad4793ef96b2fff800c5c23dbed0dfe4b354afb
Henrique Goxtoso <henrique.marqueti@estudante.ifms.edu.br>
2023-10-17 09:44:32 -0400

    commit 17/10

--- src/componentes/contato/Contato.tsx
+++ src/componentes/contato/Contato.tsx
@@ -6,6 +6,7 @@ function Contato(){
                 <h1 className="titulo">ESCREVA SUA MENSAGEM</h1>
             <input className="nome" type="text" placeholder="Nome"/>
             <input className="e-mail" type="text" placeholder="E-mail"/>
+            <input className="telefone" type="text" placeholder="Telefone" />
             <input className="campo-mensagem" type="text" placeholder="Escreva sua mensagem aqui"/>
             <button className="button-envio">Enviar</button>
             </div>
