bf0830b46cb8ea76c4df32dcc5b16f52ba3148cd
Henrique Goxtoso <henrique.marqueti@estudante.ifms.edu.br>
2023-10-18 07:16:46 -0400

    commit

--- playground-3.mongodb.js
+++ playground-3.mongodb.js
@@ -1,73 +1,65 @@
-
+use("db_sorveteria");
+db.createCollection("sorvete");
+db.sorvete.insertMany([
+  {
+    sabor:"Morango",
+    preco_kg: 25
+  },
+  {
+    sabor:"Chocolate",
+    preco_kg: 25
+  },
+{
+  sabor:"Creme",
+  preco_kg: 25
+},
+{
+  sabor:"Napolitano",
+  preco_kg: 5
+},
+  {
+    sabor:"Banana",
+    preco_kg: 10
+  }
+])
 
 //1
-use("db-patissier")
+use ("db_sorveteria")
+db.sorvete.find({});
 //2
-db.createCollection("cake")
-use("db-patissier")
-db.cake.insertMany([
-  {_id:ObjectId(1),
-    nome:"Selva Negra", 
-    peso: 18, 
-    preco: 34.00, 
-    ingredientes:[
-      {nome: "Chocolate", quantidade:"100g"},
-      {nome: "Morango", quantidade:"100g"},
-      {nome: "Leite Condensado", quantidade:"100g"}
-    ]
-  },
-  
-    {_id:ObjectId(2), 
-      nome:"Oceania", 
-      peso: 18, 
-      preco: 34.00, 
-  ingredientes:[
-    {nome: "Leite Ninho", quantidade:"100g"},
-    {nome: "Morango", quantidade:"100g"},
-    {nome: "Leite Condensado", quantidade:"100g"}
-  ]
-},
-  {_id:ObjectId(3), 
-    nome:"China", 
-    peso: 18, 
-    preco: 34.00, 
-  ingredientes:[
-  {nome: "Nesquik", quantidade:"100g"},
-  {nome: "Nuttela", quantidade:"100g"},
-  {nome: "Castanhas", quantidade:"100g"}
-]
-},
-  {_id:ObjectId(4), 
-    nome:"Aniversário", 
-    peso: 18, 
-    preco: 340.00, 
-  ingredientes:[
-    {nome: "Glasse", quantidade:"100g"},
-    {nome: "Chocolate", quantidade:"100g"},
-    {nome: "Muss de Maracujá", quantidade:"100g"}
-  ]
-}])
-
-//3 f
-use("db-patissier")
-db.cake.deleteOne({"_id": ObjectId("00000001f21ecddc3ae10039")});
+use ("db_sorveteria")
+db.sorvete.find({preco_kg: {$lte:40}});
+//3
+use ("db_sorveteria")
+db.sorvete.find({sabor: "Banana"}, {_id:1, sabor:1, preco_kg:1})
 //4
-use("db-patissier")
-db.cake.find({}, {nome: true, preco:true})
-
-
+use("db_sorveteria")
+db.sorvete.find({preco_kg: {$gte: 20}});
 
 //5
-use("db-patissier")
-db.cake.find({}, {nome:true, preco:true < 70})
-//6 
-db.cake.findOneAndUpdate()
-/*db.produtos.findOneAndUpdate(
-   { _id: ObjectId('sua_id_aqui') }, // Encontre o documento pelo ID
-   { $set: { nome: 'Novo Nome', preco: 19.99 } }, // Atualize os campos 'nome' e 'preco'
-   { returnNewDocument: true } // Retorna o documento atualizado
-);*/ 
-//Faça uma consulta que exclua a collection dono
-db.dono.drop()
-//Faça uma consulta que exclua o banco de dados mypet  
-db.dropDatabase()
+use ("db_sorveteria")
+db.sorvete.find({preco_kg: {$gte:20, $lt: 40}}, {_id:1, sabor:1});
+//6
+use ("db_sorveteria")
+db.sorvete.find({preco_kg: {$gt: 10, $lte: 44}})
+
+//7
+use("db_sorveteria")
+db.sorvete.find({$or: [{preco_kg: {$lte: 10}},{preco_kg: {$gt:40}}]})
+//8
+use("db_sorveteria")
+db.sorvete.aggregate([{
+  $group: {
+    _id: 0,
+    media: {$avg: "$preco_kg"}
+  }
+}])
+//9
+use ("db_sorveteria")
+db.sorvete.aggregate([
+  {$sort: {preco_kg: 1}}, {$limit: 1}, {$project: {_id: 0}}
+])
+//10
+use ("db_sorveteria")
+db.sorvete.insertOne({sabor:"Frutas Vermelhas", preco_kg:15})
+
